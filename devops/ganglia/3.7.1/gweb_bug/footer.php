a50033d24aeae3cfbc287e2e9c5d3aca40f268af
yhli365 <yhli365@126.com>
2015-10-10 20:57:43 +0800

    ganglia 3.7.1

---
+++ devops/ganglia/3.7.1/gweb_bug/footer.php
@@ -0,0 +1,28 @@
+<?php
+$tpl = new Dwoo_Template_File( template("footer.tpl") );
+$data = new Dwoo_Data(); 
+$data->assign("webfrontend_version",$version["webfrontend"]);
+
+#if (isset($_GET["hide-hf"]) && filter_input(INPUT_GET, "hide-hf", FILTER_VALIDATE_BOOLEAN, array("flags" => FILTER_NULL_ON_FAILURE))) {
+if (isset($_GET["hide-hf"])) {
+  $data->assign("hide_footer", true);
+}
+
+if ($version["rrdtool"]) {
+   $data->assign("rrdtool_version",$version["rrdtool"]);
+}
+
+$backend_components = array("gmetad", "gmetad-python", "gmond");
+
+foreach ($backend_components as $backend) {
+   if (isset($version[$backend])) {
+      $data->assign("webbackend_component", $backend);
+      $data->assign("webbackend_version",$version[$backend]);
+      break;
+   }
+}
+
+$data->assign("parsetime", sprintf("%.4f", $parsetime) . "s");
+
+$dwoo->output($tpl, $data);
+?>
