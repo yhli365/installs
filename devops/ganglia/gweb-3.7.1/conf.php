908eadb0eb4844b35f1056a89394fb3c3c4fcc9b
yhli365 <yhli365@126.com>
2016-09-04 17:33:59 +0800

    ganglia-web bugfixed

---
+++ devops/ganglia/gweb-3.7.1/conf.php
@@ -0,0 +1,14 @@
+<?php
+$conf['metric_groups_initially_collapsed'] = true;
+$conf['rrdcached_socket'] = "unix:/var/run/rrdcached/rrdcached.limited.sock";
+
+# By default we'll just download
+$conf['jquery_js_path'] = "js/jquery.min.js";
+$conf['jquerymobile_js_path'] = "js/jquery.mobile.min.js";
+$conf['jqueryui_js_path'] = "js/jquery-ui.min.js";
+$conf['rickshaw_js_path'] = "js/rickshaw.min.js";
+$conf['cubism_js_path'] = "js/cubism.v1.min.js";
+$conf['d3_js_path'] = "js/d3.min.js";
+$conf['protovis_js_path'] = "js/protovis.min.js";
+
+?>
